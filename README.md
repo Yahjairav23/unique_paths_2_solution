5addf42aed213c73604a33e5d2ad1cd74ac2a103
Yahjairav23 <vyahjaira23@gmail.com>
2020-09-07 20:41:24 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Unique Paths II
+
+
+If you have been struggling trying to understand how to solve the Unique Paths II coding problem on leet code, I have put together my solution with some psuedocode to hopefully help you understand what each part is doing. 
+*I will also be putting together a short video with a more in depth explanation of the solution I have here.*
+
+Here is the coding challenge solve here today: [LeetCode](https://leetcode.com/problems/unique-paths-ii/)
---
+++ unique_paths.rb
@@ -0,0 +1,27 @@
+def unique_paths_with_obstacles(obstacle_grid)
+    #find the height of the grid
+    m = obstacle_grid.length
+    #find the width of the grid
+    n = obstacle_grid.first.length
+    
+    #create an empty array that is the same width as the obstacle_grid, with 0's placed at each index
+    result = Array.new(n, 0)
+
+    #check to see if the start point is 0
+    #yes, we will set the zero index of our results array to 1
+    result[0] = 1 if obstacle_grid[0][0] == 0
+    
+    #loop through each row of the obstacle_grid
+    for i in 0...m
+        #loop through each index column of the row
+        for j in 0...n
+            #when we are at an index greater than 0, we increment our results array at the index by adding it's value to the previous index's value
+            result[j] += result[j-1] if j > 0
+            #check to se if this particular index on the Obstacle_grid has a 1 in it's place. If yes, replace our value at the results[j] with 0
+            result[j] = 0 if obstacle_grid[i][j] == 1
+        end
+    end
+    
+    # return the value for the last index of results array. This tells us all posible routes to the end point.
+    result[-1]
+end
